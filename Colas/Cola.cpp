85c9929986b42ee3415516f29062c578d71547be
CarlosArH <carlos.arranzh@edu.uah.es>
2024-10-06 15:51:18 +0200

    Add files via upload

---
+++ Colas/Cola.cpp
@@ -0,0 +1,13 @@
+#include "Cola.hpp" 
+Cola::Cola()
+{
+	primero = NULL;
+	ultimo = NULL;
+	longitud = 0;
+	
+}
+void Cola::instertar(int v)
+{
+	pnodoCola nuevo;
+	nuevo = new 
+}
---
+++ Colas/NodoPila.cpp
@@ -0,0 +1,11 @@
+#include "NodoCola.hpp"
+
+NodoCola::NodoCola(int v, NodoCola* sif)
+{
+	valor = v;
+	siguiente = sig;
+}
+NodoCola::~NodoCola()
+{
+	
+}
